2f688a3e09af2992a4db1caf9dde50e1d7226c61
Carlo Pignedoli <carlo.pignedoli@empa.ch>
2019-05-27 13:57:46 +0000

    Reactions app merged

---
+++ reactions/find_mol.py
@@ -0,0 +1,229 @@
+import numpy as np
+import itertools
+
+from ase.data import covalent_radii
+from ase.neighborlist import NeighborList
+from ase import Atoms
+
+
+
+def extract_mol_indexes_from_slab(atoms):
+    atoms.set_pbc([True,True,True])
+    exclude=['Au','Ag','Cu','Pd','Ga','Ni']
+    possible_mol_atoms=['C','N','H','O','S','F'] #problem of H making everything slow due to bottom_H
+
+    ismol=[]
+
+
+    zmin=np.min(atoms.positions[:,2])
+    all_guess_mol_atoms = [ia.index for ia in atoms if 
+                           (ia.symbol in possible_mol_atoms and ia.position[2]>zmin+2.0)]
+    
+    for ma in all_guess_mol_atoms:
+        if ma not in ismol:
+            tobeadded=all_connected_to(ma,atoms,exclude)
+            if len(tobeadded) >1:
+                for tba in tobeadded:
+                    ismol.append(tba)
+
+    return ismol
+
+
+def to_ranges(iterable):
+    iterable = sorted(set(iterable))
+    for key, group in itertools.groupby(enumerate(iterable),
+                                        lambda t: t[1] - t[0]):
+        group = list(group)
+        yield group[0][1], group[-1][1]
+        
+def mol_ids_range(ismol):
+    range_string=''
+    ranges=list(to_ranges(ismol))
+    for i in range(len(ranges)):
+        if ranges[i][1]>ranges[i][0]:
+            range_string+=str(ranges[i][0])+'..'+str(ranges[i][1])
+        else:
+            range_string+=' '+str(ranges[i][0])
+    return range_string
+
+def all_connected_to(id_atom,atoms,exclude):
+    cov_radii = [covalent_radii[a.number] for a in atoms]
+    nl = NeighborList(cov_radii, bothways = True, self_interaction = False)
+    nl.update(atoms)
+    tofollow=[]
+    followed=[]
+    isconnected=[]
+    tofollow.append(id_atom)
+    isconnected.append(id_atom)
+    while len(tofollow) > 0:
+        indices, offsets = nl.get_neighbors(tofollow[0])
+        indices=list(indices)
+        followed.append(tofollow[0])
+        for i in indices:
+            if (i not in isconnected) and (atoms[i].symbol not in exclude):
+                tofollow.append(i)
+                isconnected.append(i)
+        for i in followed:
+            if i in tofollow: ### do not remove this check
+                tofollow.remove(i)
+            #try:
+            #    tofollow.remove(i)
+            #except:
+            #    pass
+            #    
+            
+
+    return isconnected
+
+def molecules(ismol,atoms):
+    all_molecules=[]
+    to_be_checked=[i for i in range(len(ismol))]
+    all_found=[]
+    exclude=['None']
+    while len(to_be_checked) >0:
+        one_mol=all_connected_to(to_be_checked[0],atoms[ismol],exclude)
+
+        is_new_molecule = True
+        for ia in one_mol:
+            if ia in all_found:
+                is_new_molecule=False
+                break
+                
+        if is_new_molecule:
+            all_molecules.append([ismol[ia] for ia in one_mol])
+            for ia in one_mol:
+                all_found.append(ia)
+                to_be_checked.remove(ia)
+                
+            
+    return all_molecules
+
+def to_ranges(iterable):
+    iterable = sorted(set(iterable))
+    for key, group in itertools.groupby(enumerate(iterable),
+                                        lambda t: t[1] - t[0]):
+        group = list(group)
+        yield group[0][1], group[-1][1]
+        
+def mol_ids_range(ismol):
+    range_string=''
+    ranges=list(to_ranges(ismol))
+    for i in range(len(ranges)):
+        if ranges[i][1]>ranges[i][0]:
+            range_string+=str(ranges[i][0])+'..'+str(ranges[i][1])+' '
+        else:
+            range_string+=str(ranges[i][0])+' '
+    return range_string
+
+def analyze_slab(atoms):
+    atoms.set_pbc([True,True,True])
+    cov_radii = [covalent_radii[a.number] for a in atoms]
+    nl = NeighborList(cov_radii, bothways = True, self_interaction = False)
+    nl.update(atoms)
+    metalating_atoms=['Ag','Au','Cu','Co','Ni','Fe']
+    possible_slab_atoms=['Au','Ag','Cu','Pd','Ga','Ni']
+    bottom_H=[]
+    adatoms=[]
+    remaining=[]
+    metalatings=[]
+    unclassified=[]
+    slabatoms=[]
+    slab_layers=[]
+    mol_atoms=extract_mol_indexes_from_slab(atoms)
+    all_molecules=molecules(mol_atoms,atoms)
+
+    
+## bottom_H   
+    zmin=np.min(atoms.positions[:,2])
+    listh=[x[0] for x in np.argwhere(atoms.numbers == 1)]
+    for ih in listh:
+        if atoms[ih].position[2]<zmin+0.8:
+            bottom_H.append(ih)
+    
+    
+#nice but too slow and does not distinguish top from bottom    
+#    for h in listh:
+#        indices, offsets = nl.get_neighbors(h)
+#        #aus=[i for i in indices if atoms[i].symbol=='Au' ]
+#        aus=[i for i in indices if (atoms[i].symbol in possible_slab_atoms) ]
+#        for the_au in aus:
+#            iau,oau = nl.get_neighbors(the_au)
+#            h_of_one_au=[ish for ish in iau if atoms[ish].symbol=='H']
+#            if len(h_of_one_au) >1:
+#                bottom_H.append(h)
+#                break
+
+    for ra in range(len(atoms)):
+        if ra not in mol_atoms+bottom_H:
+            remaining.append(ra)
+    for ra in remaining:
+        nummetalsneigh=0
+        indices, offsets = nl.get_neighbors(ra)
+        classified=False
+        if len(indices)==0: 
+            if atoms[ra].symbol in metalating_atoms:
+                metalatings.append(ra)
+                classified=True
+        else:
+            for inra in indices:
+                if atoms[inra].symbol in possible_slab_atoms:
+                    nummetalsneigh+=1
+            if nummetalsneigh >0 and nummetalsneigh <4:
+                adatoms.append(ra)
+                classified=True
+            else:
+                slabatoms.append(ra)
+                classified=True
+        if not classified:
+            unclassified.append(ra) 
+    
+    ##slab layers
+    dz=1.0
+    ddz=0.01
+
+#    zmin different from one already computed above
+    id_zmin=np.argmin(atoms[slabatoms].positions[:,2])
+    id_zmin=slabatoms[id_zmin]
+    zmin=atoms[id_zmin].position[2]
+    possible_next_z=[ia[0] for ia in np.argwhere(atoms[slabatoms].positions[:,2] > zmin +dz)]
+    next_z=np.min(atoms[possible_next_z].positions[:,2])
+    to_be_checked=list(slabatoms)
+    while len(to_be_checked)>0:
+        #within_dz=np.logical_and(atoms.positions[:,2] > zmin -ddz , atoms.positions[:,2] <next_z -ddz  )
+        within_dz=np.logical_and(atoms[slabatoms].positions[:,2] >= zmin , atoms[slabatoms].positions[:,2] <next_z   )
+        within_dz=[slabatoms[ia[0]] for ia in np.argwhere(within_dz)]
+        slab_layers.append(within_dz)
+        for ia in within_dz:
+                to_be_checked.remove(ia)
+        if len(to_be_checked)>0:
+            id_zmin=np.argmin(atoms[to_be_checked].positions[:,2])
+            id_zmin=to_be_checked[id_zmin]
+            zmin=atoms[id_zmin].position[2]           
+            possible_next_z=[ia[0] for ia in np.argwhere(atoms.positions[:,2] > zmin +dz)]
+            next_z=np.min(atoms[possible_next_z].positions[:,2])        
+    ##end slab layers
+    
+    summary='Slab contains: \n' 
+    if len(bottom_H) >0:
+        summary+='bottom H: ' + mol_ids_range(bottom_H)   + '\n'
+    summary+='slab atoms: '   + mol_ids_range(slabatoms)  + '\n' 
+    if len(adatoms)>0:
+        summary+='adatoms: '  + mol_ids_range(adatoms)    + '\n' 
+    summary+='#'+str(len(all_molecules))   + ' molecules: ' 
+    for nmols in range(len(all_molecules)):
+        summary+=mol_ids_range(all_molecules[nmols])
+    summary+=' \n' 
+    if len(mol_ids_range(metalatings))>0:
+        summary+='metal atoms inside molecules: '+ mol_ids_range(metalatings) + '\n'
+    if len(mol_ids_range(unclassified))>0:
+        summary+='unclassified: ' + mol_ids_range(unclassified)
+    return {'slab_layers': slab_layers,
+            'bottom_H' : sorted(bottom_H),
+            'slabatoms': sorted(slabatoms),
+            'adatoms': sorted(adatoms),
+            'all_molecules': sorted(all_molecules),
+            'metalatings': sorted(metalatings),
+            'unclassified': sorted(unclassified),
+            'summary':summary
+           }
+
---
+++ reactions/nebwork.py
@@ -0,0 +1,240 @@
+from aiida.orm.data.parameter import ParameterData
+from aiida.orm.data.structure import StructureData
+from aiida.orm.data.base import Int, Str, Float, Bool, List
+from aiida.orm.data.singlefile import SinglefileData
+from aiida.orm.data.folder import FolderData
+from aiida.orm.code import Code
+from aiida.common.exceptions import NotExistent
+# from aiida.orm.data.structure import StructureData
+
+from aiida.work.workchain import WorkChain, ToContext, Calc, while_
+from aiida.work.run import submit
+
+import aiida_cp2k ##for debug print
+from aiida_cp2k.calculations import Cp2kCalculation
+
+import numpy as np
+
+import find_mol
+
+from apps.surfaces.widgets.get_cp2k_input import get_cp2k_input
+
+
+class NEBWorkchain(WorkChain):
+
+    @classmethod
+    def define(cls, spec):
+        super(NEBWorkchain, cls).define(spec)        
+        spec.input("cp2k_code"        , valid_type=Code)
+        spec.input("structure"        , valid_type=StructureData)
+        spec.input("max_force"        , valid_type=Float, default=Float(0.0005))
+        spec.input("calc_type"        , valid_type=Str, default=Str('Full DFT'))
+        spec.input("workchain"        , valid_type=Str, default=Str('NEBWorkchain'))
+        spec.input("vdw_switch"       , valid_type=Bool, default=Bool(False))
+        spec.input("mgrid_cutoff"     , valid_type=Int, default=Int(600))
+        spec.input("fixed_atoms"      , valid_type=Str, default=Str(''))        
+        spec.input("num_machines"     , valid_type=Int, default=Int(1))                
+        spec.input("struc_folder"     , valid_type=FolderData)
+        spec.input("wfn_cp_commands"  , valid_type=List)        
+        spec.input("calc_name"        , valid_type=Str)
+        spec.input("nproc_rep"        , valid_type=Int)
+        spec.input("nreplicas"        , valid_type=Int)
+        spec.input("replica_pks"      , valid_type=Str)
+        spec.input("spring"           , valid_type=Float)
+        spec.input("rotate"           , valid_type=Bool)
+        spec.input("align"            , valid_type=Bool)
+        spec.input("nstepsit"         , valid_type=Int)
+        spec.input("endpoints"        , valid_type=Bool)
+        
+        
+        spec.outline(
+            cls.init,
+            cls.calc_neb,
+            #while_(cls.not_converged)(
+            #    cls.calc_neb
+            #),
+            # cls.store_neb
+        )
+        spec.dynamic_output()
+
+    # ==========================================================================
+    def not_converged(self):
+        try:
+            self.report('Convergence check DEBUG: {}'.format(self.ctx.neb))
+            self.report('Convergence check: {}'
+                        .format(self.ctx.neb.res.exceeded_walltime))
+            return self.ctx.neb.res.exceeded_walltime
+        except AttributeError:
+            return True
+        except NotExistent:
+            return False
+
+    # ==========================================================================
+    def init(self):
+        self.report('Init NEB')
+        # Set the restart folder
+        try:
+            self.ctx.remote_calc_folder = self.ctx.neb.remote_calc_folder
+        except AttributeError:
+            self.ctx.remote_calc_folder = None
+
+        # Here we need to create the xyz files of all the replicas
+        #self.ctx.this_name = self.inputs.calc_name
+        self.ctx.file_list = self.inputs.struc_folder.get_folder_list()
+        self.ctx.n_files = len(self.ctx.file_list) #-2
+
+        # Report some things
+        self.report('Passed #{} replica geometries + files'.format(self.ctx.n_files))
+        self.report('Replicas: {}'.format(self.ctx.file_list))
+
+    # ==========================================================================
+    def calc_neb(self):
+        self.report("Running CP2K CI-NEB calculation."
+                    .format(self.inputs.calc_name))
+        
+        inputs = self.build_calc_inputs(cp2k_code          = self.inputs.cp2k_code,      
+                                        structure          = self.inputs.structure ,     
+                                        max_force          = self.inputs.max_force,      
+                                        calc_type          = self.inputs.calc_type,      
+                                        workchain          = self.inputs.workchain,      
+                                        vdw_switch         = self.inputs.vdw_switch,     
+                                        mgrid_cutoff       = self.inputs.mgrid_cutoff,   
+                                        fixed_atoms        = self.inputs.fixed_atoms,    
+                                        num_machines       = self.inputs.num_machines,   
+                                        struc_folder       = self.inputs.struc_folder,   
+                                        wfn_cp_commands    = self.inputs.wfn_cp_commands,
+                                        nproc_rep          = self.inputs.nproc_rep,      
+                                        nreplicas          = self.inputs.nreplicas, 
+                                        replica_pks        = self.inputs.replica_pks,
+                                        spring             = self.inputs.spring,         
+                                        rotate             = self.inputs.rotate,         
+                                        align              = self.inputs.align,          
+                                        nstepsit           = self.inputs.nstepsit,       
+                                        endpoints          = self.inputs.endpoints, 
+                                        file_list          = self.ctx.file_list,
+                                        remote_calc_folder = self.ctx.remote_calc_folder
+                                       )
+        self.report(" ")
+        self.report("inputs: "+str(inputs))
+        self.report(" ")
+        self.report("Using aiida-cp2k: "+str(aiida_cp2k.__file__))
+        self.report(" ")
+        future = submit(Cp2kCalculation.process(), **inputs)
+        self.report("future: "+str(future))
+        self.report(" ")
+        return ToContext(neb=Calc(future))
+    # ==========================================================================
+    #def store_replica(self):structure =
+    #    return self.out('replica_{}'.format(self.ctx.this_name),
+    #                    self.ctx.neb.out.output_structure)
+    # ==========================================================================
+    @classmethod
+    def build_calc_inputs(cls, 
+                          cp2k_code          = None,
+                          structure          = None,
+                          max_force          = None,
+                          calc_type          = None,
+                          workchain          = None,
+                          vdw_switch         = None,
+                          mgrid_cutoff       = None,
+                          fixed_atoms        = None,
+                          num_machines       = None,
+                          struc_folder       = None,
+                          wfn_cp_commands    = None,
+                          nproc_rep          = None,
+                          nreplicas          = None,
+                          replica_pks        = None,
+                          spring             = None,
+                          rotate             = None,
+                          align              = None,
+                          nstepsit           = None,
+                          endpoints          = None,
+                          file_list          = None,
+                          remote_calc_folder = None,
+                          **not_used
+                         ): 
+
+        inputs = {}
+        inputs['_label'] = "NEB"
+
+        inputs['code'] = cp2k_code
+        inputs['file'] = {}
+        atoms = structure.get_ase()  # slow
+        
+        cell=[atoms.cell[0, 0],atoms.cell[0, 1], atoms.cell[0, 2],
+              atoms.cell[1, 0],atoms.cell[1, 1], atoms.cell[1, 2],
+              atoms.cell[2, 0],atoms.cell[2, 1], atoms.cell[2, 2]]
+        
+        # The files passed by the notebook
+
+        for f in struc_folder.get_folder_list():
+            path = struc_folder.get_abs_path()+'/path/'+f
+            inputs['file'][f] = SinglefileData(file=path)
+
+
+        remote_computer = cp2k_code.get_remote_computer()
+        machine_cores = remote_computer.get_default_mpiprocs_per_machine()
+        first_slab_atom = None
+        
+        if calc_type != 'Full DFT':
+            
+            # Au potential
+            pot_f = SinglefileData(file='/project/apps/surfaces/slab/Au.pot')
+            inputs['file']['au_pot'] = pot_f
+            mol_indexes = find_mol.extract_mol_indexes_from_slab(atoms)
+            
+            first_slab_atom = len(mol_indexes) + 1
+            
+        if calc_type == 'Mixed DFTB':
+            walltime = 18000
+        else:
+            walltime = 86000
+
+        nreplica_files=replica_pks.value
+        nreplica_files=len(nreplica_files.split())
+        inp = get_cp2k_input(atoms=atoms,
+                             cell=cell,
+                             fixed_atoms=fixed_atoms,
+                             max_force=max_force,
+                             machine_cores=machine_cores*num_machines,
+                             align=align,
+                             endpoints=endpoints,
+                             nproc_rep=nproc_rep,
+                             nreplicas=nreplicas,
+                             nstepsit=nstepsit,
+                             rotate=rotate,
+                             spring=spring,
+                             calc_type=calc_type,
+                             mgrid_cutoff=mgrid_cutoff,
+                             nreplica_files=nreplica_files,
+                             first_slab_atom=first_slab_atom,
+                             last_slab_atom=len(atoms),
+                             walltime=walltime*0.97,
+                             workchain=workchain,
+                             remote_calc_folder=remote_calc_folder
+                            )
+
+        if remote_calc_folder is not None:
+            inputs['parent_folder'] = remote_calc_folder
+
+        inputs['parameters'] = ParameterData(dict=inp)
+
+        # settings
+        settings = ParameterData(dict={'additional_retrieve_list': ['*.xyz',
+                                                                    '*.out',
+                                                                    '*.ener']})
+        inputs['settings'] = settings
+
+        # resources
+        inputs['_options'] = {
+            "resources": {"num_machines": num_machines},
+            "max_wallclock_seconds": walltime,
+        }
+        if len(wfn_cp_commands) > 0:
+            inputs['_options']["prepend_text"] = ""
+            for wfn_cp_command in wfn_cp_commands:
+                inputs['_options']["prepend_text"] += wfn_cp_command + "\n"
+        return inputs
+
+    # ==========================================================================
+
