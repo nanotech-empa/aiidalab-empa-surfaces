8ae5872faf940c4497eb7e2baff9f8002568824a
Carlo Pignedoli <carlo.pignedoli@empa.ch>
2019-05-27 13:59:05 +0000

    start page update

--- start.py
+++ start.py
@@ -6,20 +6,55 @@ def get_start_widget(appbase, jupbase):
     <table>
     <tr>
         <th style="text-align:center">Structures</th>
-        <th style="width:70px" rowspan=2></th>
+        <th style="width:60px" rowspan=2></th>
         <th style="text-align:center">Slab Models</th>
+        <th style="width:60px" rowspan=2></th>
+        <th style="text-align:center">Molecules</th>
+        <th style="width:60px" rowspan=2></th>
+        <th style="text-align:center">Bulks</th>
         
     <tr>
     <td valign="top"><ul>
-    <li><a href="{appbase}/upload_structure.ipynb" target="_blank">Upload structures</a>
-    <li><a href="{appbase}/edit_structure.ipynb" target="_blank">Assign spin, remove atoms</a>
+        <li><a href="{appbase}/upload_structure.ipynb" target="_blank">Upload structures</a>
+        <li><a href="{appbase}/edit_structure.ipynb" target="_blank">Assign spin, remove atoms</a>
     </ul></td>
     
     <td valign="top"><ul>
-    <li><a href="{appbase}/slab/build.ipynb" target="_blank">Construct slab</a>
-    <li><a href="{appbase}/slab/submit_geopt.ipynb" target="_blank">Submit geo-opt</a>
-    <li><a href="{appbase}/slab/search.ipynb" target="_blank">Search database</a>
-    <li><a href="{appbase}/slab/submit_adsorption.ipynb" target="_blank">Adsorption energy</a>
+        <li><a href="{appbase}/slab/build.ipynb" target="_blank">Construct slab</a>
+        <li><a href="{appbase}/slab/submit_geopt.ipynb" target="_blank">Submit geo-opt</a>
+        <li><a href="{appbase}/slab/search.ipynb" target="_blank">Search database</a>
+        <li><a href="{appbase}/slab/submit_adsorption.ipynb" target="_blank">Adsorption energy</a>
+    </ul></td>
+
+    <td valign="top"><ul>
+        <li><a href="{appbase}/molecules/submit_geoopt.ipynb" target="_blank">Submit Geo Opt</a>
+        <li><a href="{appbase}/molecules/submit_gw.ipynb" target="_blank">Submit GW or GW+IC</a>
+        <li><a href="{appbase}/molecules/search.ipynb" target="_blank">Search Optimized molecules</a>
+    </ul></td>
+
+    <td valign="top"><ul>
+        <li><a href="{appbase}/bulks/upload_many_structures.ipynb" target="_blank">Upload Structures</a>
+        <li><a href="{appbase}/bulks/submit_cellopt.ipynb" target="_blank">Submit Cell Opt</a>
+        <li><a href="{appbase}/bulks/submit_bulkopt.ipynb" target="_blank">Submit Bulk Opt</a>
+        <li><a href="{appbase}/bulks/search.ipynb" target="_blank">Search Cell Opt</a>
+    </ul></td>
+
+    </tr></table>
+
+    <table>
+    <tr>
+        <th style="text-align:center">Replicas</th>
+        <th style="width:60px" rowspan=2></th>
+        <th style="text-align:center">CI-NEB</th>
+    </tr>
+    <tr>
+    <td valign="top"><ul>
+        <li><a href="{appbase}/reactions/replicas.ipynb" target="_blank">Generate replicas</a>
+        <li><a href="{appbase}/reactions/SearchReplica.ipynb" target="_blank">Search Replicas</a>
+    </ul></td>
+    <td><ul>
+        <li><a href="{appbase}/reactions/NEB.ipynb" target="_blank">NEB</a>
+        <li><a href="{appbase}/reactions/SearchNEB.ipynb" target="_blank">Search NEBs</a>
     </ul></td>
     </tr></table>
 """
