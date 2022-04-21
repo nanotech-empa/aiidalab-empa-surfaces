5b4f8be7172202da2af6fb35ada9d5375dba14a8
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2022-04-21 15:43:51 +0200

    Clean up the repository: remove unused files. (#95)

--- start.py
+++ start.py
@@ -17,7 +17,6 @@ def get_start_widget(appbase, jupbase):
     <td valign="top"><ul>
         <li><a href="{appbase}/submit_calculations.ipynb" target="_blank">Submit optimizatons</a>
         <li><a href="{appbase}/gw/submit_gw.ipynb" target="_blank">Submit GW</a>
-        <li><a href="{appbase}/slab/build_slab.ipynb" target="_blank">Build slab</a>
         <li><a href="{appbase}/search.ipynb" target="_blank">Search</a>
     </ul></td>
     
