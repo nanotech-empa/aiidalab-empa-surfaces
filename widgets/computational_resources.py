904ddd77f695e2251d99e5bdb4f5ac79acc4b1ee
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2022-10-17 12:39:31 +0000

    Make sure resources and resources_estimator work for geo opt notebook.

--- widgets/computational_resources.py
+++ widgets/computational_resources.py
@@ -1,10 +1,9 @@
-import traitlets as trt
 import ipywidgets as ipw
+import pandas as pd
+import traitlets as trt
 from aiida import orm
 
-
 STYLE = {"description_width": "120px"}
-LAYOUT = {"width": "35%"}
 
 
 class ProcessResourcesWidget(ipw.VBox):
@@ -14,15 +13,21 @@ class ProcessResourcesWidget(ipw.VBox):
         """Metadata widget to generate metadata"""
 
         self.walltime = ipw.Text(
-            value="86400",
             description="Walltime:",
-            style={"description_width": "initial"},
-            layout={"width": "initial"},
+            placeholder="10:30:00",
+            style=STYLE,
         )
 
-        self.nodes = ipw.IntText(
-            value=48, description="# Nodes", style=STYLE, layout=LAYOUT
+        self.wrong_syntax = ipw.HTML(
+            value="""<i class="fa fa-times" style="color:red;font-size:2em;" ></i> wrong syntax""",
+            layout={"visibility": "hidden"},
         )
+        self.time_info = ipw.HTML()
+        self.walltime.observe(self.parse_time_string, "value")
+
+        self.walltime.value = "10:00:00"
+
+        self.nodes = ipw.IntText(value=48, description="# Nodes", style=STYLE)
         self.tasks_per_node = ipw.IntText(
             value=12, description="# Tasks per node", style=STYLE
         )
@@ -30,12 +35,11 @@ class ProcessResourcesWidget(ipw.VBox):
             value=1, description="# Threads per task", style=STYLE
         )
 
-
         children = [
             self.nodes,
             self.tasks_per_node,
             self.threads_per_task,
-            self.walltime,
+            ipw.HBox([self.walltime, self.time_info, self.wrong_syntax]),
         ]
 
         super().__init__(children=children)
@@ -47,9 +51,19 @@ class ProcessResourcesWidget(ipw.VBox):
             "nodes": self.nodes.value,
             "tasks_per_node": self.tasks_per_node.value,
             "threads_per_task": self.threads_per_task.value,
-            "walltime": self.walltime_s.value,
+            "walltime": self.walltime.value,
         }
 
+    def parse_time_string(self, _=None):
+        """Parse the time string and set the time in seconds"""
+        self.wrong_syntax.layout.visibility = "hidden"
+        self.time_info.value = ""
+        try:
+            dtime = pd.Timedelta(self.walltime.value)
+            self.time_info.value = str(dtime)
+        except Exception:
+            self.wrong_syntax.layout.visibility = "visible"
+            self.time_info.value = ""
 
 
 class ResourcesEstimatorWidget(ipw.VBox):
@@ -61,47 +75,40 @@ class ResourcesEstimatorWidget(ipw.VBox):
     def __init__(self):
         """Resources estimator widget to generate metadata"""
 
-        self.estimate_nodes_button = ipw.Button(
+        self.estimate_resources_button = ipw.Button(
             description="Estimate resources", button_style="warning"
         )
-        self.estimate_nodes_button.on_click(self._suggest_resources)
+        self.estimate_resources_button.on_click(self._estimate_resources)
 
         super().__init__([self.estimate_resources_button])
 
-    def _suggest_resources(self, _=None):
+    def link_to_resources_widget(self, resources_widget):
+        self.resources = resources_widget
+
+    @trt.observe("details")
+    def _observe_details(self, _=None):
         try:
-            max_tasks_per_node = (
+            self.systype = (
+                "Slab"
+                if "Slab" in self.details["system_type"]
+                else self.details["system_type"]
+            )
+            self.element_list = self.details["all_elements"]
+        except KeyError:
+            self.systype = "Other"
+            self.element_list = []
+        self.calctype = self.systype + "-DFT"
+
+    @trt.observe("selected_code")
+    def _observe_code(self, _=None):
+        try:
+            self.max_tasks_per_node = (
                 self.selected_code.computer.get_default_mpiprocs_per_machine()
             )
         except AttributeError:
-            max_tasks_per_node = None
-        if max_tasks_per_node is None:
-            max_tasks_per_node = 1
+            self.max_tasks_per_node = 1
 
-        try:
-            systype = self.details["system_type"]
-            element_list = self.details["all_elements"]
-        except KeyError:
-            systype = "Other"
-            element_list = []
-
-        if "Slab" in systype:
-            systype = "Slab"
-        calctype = systype + "-DFT"
-
-        (
-            self.nodes.value,
-            self.tasks_per_node.value,
-            self.threads_per_task.value,
-        ) = get_nodes(
-            element_list=element_list,
-            calctype=calctype,
-            systype=systype,
-            max_tasks_per_node=max_tasks_per_node,
-            uks=False,
-        )
-
-    def compute_cost(element_list=None, systype="Slab", uks=False):
+    def _compute_cost(self, element_list=None, systype="Slab", uks=False):
         cost = {
             "H": 1,
             "C": 4,
@@ -136,107 +143,194 @@ class ResourcesEstimatorWidget(ipw.VBox):
                 the_cost = the_cost * 1.26
         return the_cost
 
-
-    def get_nodes(
-        element_list=None,
-        calctype="default",
-        systype="Slab",
-        max_tasks_per_node=1,
-        uks=False,
-    ):
-        """ "Determine the resources needed for the calculation."""
+    def _estimate_resources(self, _=None):
+        """Determine the resources needed for the calculation."""
 
         resources = {
             "Slab-DFT": {
-                50: {"nodes": 4, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                200: {"nodes": 12, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                1400: {"nodes": 27, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                3000: {"nodes": 48, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                4000: {"nodes": 75, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                10000: {"nodes": 108, "tasks_per_node": max_tasks_per_node, "threads": 1},
+                50: {
+                    "nodes": 4,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                200: {
+                    "nodes": 12,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                1400: {
+                    "nodes": 27,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                3000: {
+                    "nodes": 48,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                4000: {
+                    "nodes": 75,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                10000: {
+                    "nodes": 108,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
             },
             "Bulk-DFT": {
-                50: {"nodes": 4, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                200: {"nodes": 12, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                1400: {"nodes": 27, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                3000: {"nodes": 48, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                4000: {"nodes": 75, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                10000: {"nodes": 108, "tasks_per_node": max_tasks_per_node, "threads": 1},
+                50: {
+                    "nodes": 4,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                200: {
+                    "nodes": 12,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                1400: {
+                    "nodes": 27,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                3000: {
+                    "nodes": 48,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                4000: {
+                    "nodes": 75,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                10000: {
+                    "nodes": 108,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
             },
             "Molecule-DFT": {
-                50: {"nodes": 4, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                100: {"nodes": 12, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                180: {"nodes": 27, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                400: {"nodes": 48, "tasks_per_node": max_tasks_per_node, "threads": 1},
+                50: {
+                    "nodes": 4,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                100: {
+                    "nodes": 12,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                180: {
+                    "nodes": 27,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                400: {
+                    "nodes": 48,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
             },
             "Other-DFT": {
-                50: {"nodes": 4, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                100: {"nodes": 12, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                180: {"nodes": 27, "tasks_per_node": max_tasks_per_node, "threads": 1},
-                400: {"nodes": 48, "tasks_per_node": max_tasks_per_node, "threads": 1},
+                50: {
+                    "nodes": 4,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                100: {
+                    "nodes": 12,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                180: {
+                    "nodes": 27,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
+                400: {
+                    "nodes": 48,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
             },
             "gw": {
                 10: {
                     "nodes": 2,
-                    "tasks_per_node": max(max_tasks_per_node / 4, 1),
+                    "tasks_per_node": max(self.max_tasks_per_node / 4, 1),
                     "threads": 1,
                 },
                 20: {
                     "nodes": 6,
-                    "tasks_per_node": max(max_tasks_per_node / 4, 1),
+                    "tasks_per_node": max(self.max_tasks_per_node / 4, 1),
                     "threads": 1,
                 },
-                50: {"nodes": 12, "tasks_per_node": max_tasks_per_node, "threads": 1},
+                50: {
+                    "nodes": 12,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
                 100: {
                     "nodes": 256,
-                    "tasks_per_node": int(max(max_tasks_per_node / 3, 1)),
+                    "tasks_per_node": int(max(self.max_tasks_per_node / 3, 1)),
                     "threads": 1,
                 },
                 180: {
                     "nodes": 512,
-                    "tasks_per_node": int(max(max_tasks_per_node / 3, 1)),
+                    "tasks_per_node": int(max(self.max_tasks_per_node / 3, 1)),
                     "threads": 1,
                 },
                 400: {
                     "nodes": 1024,
-                    "tasks_per_node": int(max(max_tasks_per_node / 3, 1)),
+                    "tasks_per_node": int(max(self.max_tasks_per_node / 3, 1)),
                     "threads": 1,
                 },
             },
             "gw_ic": {
                 10: {
                     "nodes": 2,
-                    "tasks_per_node": max(max_tasks_per_node / 4, 1),
+                    "tasks_per_node": max(self.max_tasks_per_node / 4, 1),
                     "threads": 1,
                 },
                 20: {
                     "nodes": 6,
-                    "tasks_per_node": max(max_tasks_per_node / 4, 1),
+                    "tasks_per_node": max(self.max_tasks_per_node / 4, 1),
                     "threads": 1,
                 },
-                50: {"nodes": 12, "tasks_per_node": max_tasks_per_node, "threads": 1},
+                50: {
+                    "nodes": 12,
+                    "tasks_per_node": self.max_tasks_per_node,
+                    "threads": 1,
+                },
                 100: {
                     "nodes": 256,
-                    "tasks_per_node": int(max(max_tasks_per_node / 3, 1)),
+                    "tasks_per_node": int(max(self.max_tasks_per_node / 3, 1)),
                     "threads": 1,
                 },
                 180: {
                     "nodes": 512,
-                    "tasks_per_node": int(max(max_tasks_per_node / 3, 1)),
+                    "tasks_per_node": int(max(self.max_tasks_per_node / 3, 1)),
                     "threads": 1,
                 },
                 400: {
                     "nodes": 1024,
-                    "tasks_per_node": int(max(max_tasks_per_node / 3, 1)),
+                    "tasks_per_node": int(max(self.max_tasks_per_node / 3, 1)),
                     "threads": 1,
                 },
             },
         }
 
-        cost = compute_cost(element_list=element_list, systype=systype, uks=uks)
+        cost = self._compute_cost(
+            element_list=self.element_list, systype=self.systype, uks=self.uks
+        )
 
-        theone = min(resources[calctype], key=lambda x: abs(x - cost))
-        nodes = resources[calctype][theone]["nodes"]
-        tasks_per_node = resources[calctype][theone]["tasks_per_node"]
-        threads = resources[calctype][theone]["threads"]
-        return nodes, tasks_per_node, threads
+        theone = min(resources[self.calctype], key=lambda x: abs(x - cost))
+
+        self.resources.nodes.value = resources[self.calctype][theone]["nodes"]
+        self.resources.tasks_per_node.value = resources[self.calctype][theone][
+            "tasks_per_node"
+        ]
+        self.resources.threads_per_task.value = resources[self.calctype][theone][
+            "threads"
+        ]
