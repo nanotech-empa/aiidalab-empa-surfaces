9887ba81cc34525b308795b3938055f9a1b349b2
Carlo Pignedoli <c.pignedoli@gmail.com>
2023-02-19 17:05:30 +0000

    adapted parser to new neb

--- reactions/cp2k_neb_parser.py
+++ reactions/cp2k_neb_parser.py
@@ -128,11 +128,14 @@ class Cp2kNebParser(Parser):
         ]
         coord_set_with_elements = coord_line_sets[-1]
         replica_coord_line_sets = coord_line_sets[:-1]
-        # remove integers from element names
+        # remove integers from element names and create tags
         element_list = [re.sub(r'[0-9]+', '', line[0]) for line in coord_set_with_elements]
+        tags = [int(re.findall(r'\d+', line[0])[0]) for line in coord_set_with_elements]
+
         for i_rep, rep_coord_lines in enumerate(replica_coord_line_sets):
             positions = np.array(rep_coord_lines, np.float64)
             ase_atoms = ase.Atoms(symbols=element_list, positions=positions, cell=cell)
+            ase_atoms.set_tags(tags)
 
             self.out(
                 "opt_replica_%s" % str(i_rep).zfill(3), StructureData(ase=ase_atoms)
