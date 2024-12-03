e1233ff6e4f06b2a263b40e346fd4e97caa4705a
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2024-12-03 09:14:00 +0000

    initial commit

--- start.py
+++ start.py
@@ -1,47 +1,60 @@
 import ipywidgets as ipw
 
 
+import ipywidgets as ipw
+
 def get_start_widget(appbase, jupbase):
-    # http://fontawesome.io/icons/
-    template = """
+    # Template for displaying icons with links
+    template = f"""
     <table>
     <tr>
-        <th style="text-align:center">Density functional theory</th>
+        <th style="text-align:center">Density Functional Theory</th>
         <th style="width:60px" rowspan=2></th>
         <th style="text-align:center">Post-processing</th>
-        <!--
-        <th style="width:60px" rowspan=2></th>
-        <th style="text-align:center">GW</th>
-        -->
     </tr>
 
     <tr>
+    <td valign="top">
+        <div style="display: flex; flex-direction: column; align-items: center; gap: 15px;">
+            <a href="{appbase}/submit_geometry_optimization.ipynb" target="_blank">
+                <img src="{appbase}/buttons/geo_opt.png?raw=true" height="60px" width="120px" style="cursor: pointer;" alt="Geometry Optimization">
+                <div>Geometry Optimization</div>
+            </a>
+            <a href="{appbase}/submit_adsorption_energy.ipynb" target="_blank">
+                <img src="{appbase}/buttons/ads_ene.png?raw=true" height="60px" width="120px" style="cursor: pointer;" alt="Adsorption Energy">
+                <div>Adsorption Energy</div>
+            </a>
+            <a href="{appbase}/submit_phonons.ipynb" target="_blank">
+                <img src="{appbase}/buttons/phonons.png?raw=true" height="60px" width="120px" style="cursor: pointer;" alt="Phonons">
+                <div>Phonons</div>
+            </a>
+            <a href="{appbase}/submit_reactions.ipynb" target="_blank">
+                <img src="{appbase}/buttons/ts_search.png?raw=true" height="60px" width="120px" style="cursor: pointer;" alt="Transition State Search">
+                <div>Transition State Search</div>
+            </a>
+            <a href="{appbase}/search.ipynb" target="_blank">
+                <img src="{appbase}/buttons/search.png?raw=true" height="60px" width="120px" style="cursor: pointer;" alt="Search">
+                <div>Search</div>
+            </a>
+        </div>
+    </td>
 
-    <td valign="top"><ul>
-        <li><a href="{appbase}/submit_geometry_optimization.ipynb" target="_blank">Geometry optimization</a>
-        <li><a href="{appbase}/submit_adsorption_energy.ipynb" target="_blank">Adsorption energy</a>
-        <li><a href="{appbase}/submit_phonons.ipynb" target="_blank">Phonons</a>
-        <li><a href="{appbase}/submit_reactions.ipynb" target="_blank">Reactions</a>
-        <li><a href="{appbase}/search.ipynb" target="_blank">Search</a>
-    </ul></td>
-
-    <td valign="top"><ul>
-        <li><a href="{appbase}/submit_spm.ipynb" target="_blank">Scanning probe microscopy</a>
-        <li><a href="{appbase}/submit_pdos.ipynb" target="_blank">Projected density of states</a>
-    </ul></td>
-
-    <!--
-    <td valign="top"><ul>
-        <li><a href="{appbase}/submit_gw.ipynb" target="_blank">GW</a>
-        <li><a href="{appbase}/submit_gw_ic.ipynb" target="_blank">GW-IC</a>
-    </ul></td>
-    -->
-
+    <td valign="top">
+        <div style="display: flex; flex-direction: column; align-items: center; gap: 15px;">
+            <a href="{appbase}/submit_spm.ipynb" target="_blank">
+                <img src="{appbase}/buttons/spm.png?raw=true" height="60px" width="120px" style="cursor: pointer;" alt="Scanning Probe Microscopy">
+                <div>Scanning Probe Microscopy</div>
+            </a>
+            <a href="{appbase}/submit_pdos.ipynb" target="_blank">
+                <img src="{appbase}/buttons/pdos.png?raw=true" height="60px" width="120px" style="cursor: pointer;" alt="Projected Density of States">
+                <div>Projected Density of States</div>
+            </a>
+        </div>
+    </td>
     </tr>
-
     </table>
-
     """
 
     html = template.format(appbase=appbase, jupbase=jupbase)
     return ipw.HTML(html)
+
