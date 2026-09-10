31a2de209fd1ea2f3c8469f7fb49a9c8fd5e5d17
Carlo Antonio Pignedoli <c.pignedoli@gmail.com>
2026-09-10 10:54:07 +0000

    Ignore incomplete PDOS selections

--- tests/test_pdos_export.py
+++ tests/test_pdos_export.py
@@ -2,8 +2,9 @@ import base64
 import re
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-from surfaces_tools.widgets.pdos import make_image_link
+from surfaces_tools.widgets.pdos import PdosOverlapViewerWidget, make_image_link
 
 
 def test_make_svg_image_link_contains_vector_figure():
@@ -24,3 +25,22 @@ def test_make_svg_image_link_contains_vector_figure():
     svg = base64.b64decode(match.group(1)).decode()
     assert "<svg" in svg
     assert "PDOS" in svg
+
+
+def test_plot_projections_ignores_empty_selection():
+    viewer = PdosOverlapViewerWidget()
+    viewer._projections.add_item(None)
+    energy = np.array([0.0, 1.0])
+    collected_data = energy.reshape(1, -1)
+    headers = ["energy [eV]"]
+    fig, ax = plt.subplots()
+
+    try:
+        result_headers, result_data = viewer._plot_projections(
+            ax, [None, None], energy, collected_data, headers
+        )
+    finally:
+        plt.close(fig)
+
+    assert result_headers == headers
+    np.testing.assert_array_equal(result_data, collected_data)
